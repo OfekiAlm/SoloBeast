754a620fda682968f8f2472935d4bbd0b7dd7cf5
Ofek <ofekalm100@gmail.com>
2022-12-05 11:00:40 +0200

    added app bar with clickable icon

--- app/src/main/java/com/example/solobeast/MainActivity.java
+++ app/src/main/java/com/example/solobeast/MainActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
    // private String username = null;
     private TextView DisplayUsername;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +21,25 @@ public class MainActivity extends AppCompatActivity {
         init();
 //        Bundle extras = getIntent().getExtras();
 //        username = (String) extras.get("username_of_user");
-        String data = getIntent().getExtras().getString("username_of_user","User");
-        DisplayUsername.setText("Hello world " + data);
+        //String data = getIntent().getExtras().getString("username_of_user","User");
+        //DisplayUsername.setText("Hello world " + data);
 
 //        String[] username_split=username.split("@");
 //        String username_sofi = username_split[0];
         //DisplayUsername.setText("Hello world " + username);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"your icon was clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
     private void init(){
-       DisplayUsername= findViewById(R.id.display_tv);
+       //DisplayUsername= findViewById(R.id.display_tv);
     }
 }
