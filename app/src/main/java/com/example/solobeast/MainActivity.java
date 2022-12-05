a73a5a07bd13e75153494c57e72bdfa863dc7c49
Ofek <ofekalm100@gmail.com>
2022-12-06 00:05:07 +0200

    navigation drawer with clickable items

--- app/src/main/java/com/example/solobeast/MainActivity.java
+++ app/src/main/java/com/example/solobeast/MainActivity.java
@@ -1,24 +1,35 @@
 package com.example.solobeast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
    // private String username = null;
     private TextView DisplayUsername;
-    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        navdrawer_init();
 //        Bundle extras = getIntent().getExtras();
 //        username = (String) extras.get("username_of_user");
         //String data = getIntent().getExtras().getString("username_of_user","User");
@@ -27,19 +38,59 @@ public class MainActivity extends AppCompatActivity {
 //        String[] username_split=username.split("@");
 //        String username_sofi = username_split[0];
         //DisplayUsername.setText("Hello world " + username);
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
 
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"your icon was clicked",Toast.LENGTH_SHORT).show();
-            }
-        });
+
 
     }
     private void init(){
        //DisplayUsername= findViewById(R.id.display_tv);
+    }
+    private void navdrawer_init(){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.action_nav_open, R.string.action_nav_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.nav_settings:
+                Log.i("MoveScreen", "MoveToSettingsScreen:Success");
+                break;
+            case R.id.nav_onboarding:
+                Log.i("MoveScreen", "MoveToOnBoardScreen:Success");
+                break;
+            case R.id.nav_contact:
+                Log.i("MoveScreen", "MoveToContact  Screen:Success");
+                break;
+            case R.id.nav_donate:
+                Log.i("MoveScreen", "MoveToDonateScreen:Success");
+                break;
+            case R.id.nav_auth:
+                Log.i("AuthData", "LogOut:Success");
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
