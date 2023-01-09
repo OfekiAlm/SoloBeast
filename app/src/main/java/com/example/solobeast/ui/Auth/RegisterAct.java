793945a2f8eafd15e8fb640104e7488e6e96fb92
Ofeka <ofekalm100@gmail.com>
2023-01-09 15:58:41 +0200

    added upload image to firebaseStorage

--- app/src/main/java/com/example/solobeast/ui/Auth/RegisterAct.java
+++ app/src/main/java/com/example/solobeast/ui/Auth/RegisterAct.java
@@ -1,15 +1,13 @@
 package com.example.solobeast.ui.Auth;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -19,6 +17,7 @@ import android.widget.Toast;
 
 import com.example.solobeast.Objects.User;
 import com.example.solobeast.R;
+import com.example.solobeast.ui.Home.MainActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -27,6 +26,10 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.ByteArrayOutputStream;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -35,9 +38,10 @@ public class RegisterAct extends AppCompatActivity {
     private String TAG = "AuthData";
     private FirebaseAuth mAuth;
     ActivityResultLauncher<Intent> activityResultLauncher;
-
+    StorageReference storageReference;
     FloatingActionButton takePicBtn;
     CircleImageView circleImageView;
+    Bitmap imageBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,12 +49,11 @@ public class RegisterAct extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         mAuth = FirebaseAuth.getInstance();
         init();
-
+        RegisterOnResult_and_takePicFromCamera();
         takePicBtn.setOnClickListener(view -> {
             Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             activityResultLauncher.launch(i);
         });
-        takePicFromCamera();
     }
 
     private void init(){
@@ -58,8 +61,6 @@ public class RegisterAct extends AppCompatActivity {
         editTextpassword = findViewById(R.id.editTextTextPassword_Register);
         circleImageView = findViewById(R.id.profile_image);
         takePicBtn = findViewById(R.id.profile_pic_fab);
-
-
 
 
         TextView moveToLogin = findViewById(R.id.move_screen);
@@ -81,17 +82,19 @@ public class RegisterAct extends AppCompatActivity {
             editTextpassword.requestFocus();
             return false;
         }
+
         return true;
     }
-    private void takePicFromCamera(){
+    private void RegisterOnResult_and_takePicFromCamera(){
         activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
                 result -> {
                     if (result.getResultCode() == RESULT_OK && result.getData() != null) {
                         Bitmap bitmap = (Bitmap) result.getData().getExtras().get("data");
                         circleImageView.setImageBitmap(bitmap);
+                        imageBitmap = bitmap;
                     }
                     else if(result.getResultCode() == RESULT_CANCELED){
-                        Toast.makeText(getBaseContext(),"You CANCELED the picture",Toast.LENGTH_LONG).show();
+                        Toast.makeText(getBaseContext(),"You canceled the picture",Toast.LENGTH_LONG).show();
                     }
                 });
     }
@@ -126,7 +129,9 @@ public class RegisterAct extends AppCompatActivity {
                                             }
                                         });
 
-                                updateUI(user);
+                                uploadImageToFirebaseStorage(imageBitmap);
+
+                                updateUI();
                             } else {
                                 // If sign in fails, display a message to the user.
                                 Log.w(TAG, "createUserWithEmail:failure", task.getException());
@@ -138,9 +143,31 @@ public class RegisterAct extends AppCompatActivity {
                     });
         }
     }
+    private void uploadImageToFirebaseStorage(Bitmap bitmap){
+            storageReference = FirebaseStorage.getInstance().getReference();
+            //CONVERTS TO PNG FILE\\
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            byte[] data = stream.toByteArray();
+            //\\
+            StorageReference imageRef = storageReference.child("profile-images/" + FirebaseAuth.getInstance().getCurrentUser().getUid());
+            Task<Uri> urlTask = imageRef.putBytes(data).continueWithTask(task -> {
+                if (!task.isSuccessful()) {
+                    throw task.getException();
+                }
+                if (task.isSuccessful()) {
+                    Toast.makeText(this,"Image uploaded to database",Toast.LENGTH_LONG).show();
+                } else {
+                    // Handle failures
+                    // ...
+                }
+                return imageRef.getDownloadUrl();
+            });
+    }
+    private void updateUI() {
+        Intent i = new Intent(this, MainActivity.class);
 
-    private void updateUI(FirebaseUser user) {
-
+        startActivity(i);
     }
 
 }
